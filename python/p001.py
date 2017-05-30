5f28f0dc9633251250db5df2fcda7708c4ba4905
Diego J <diegojromerolopez@gmail.com>
2017-05-31 01:12:52 +0200

    First two problems in Python

---
+++ python/p001.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+from __future__ import unicode_literals, print_function
+
+
+def multiples_of_3_and_5(below_limit):
+    for num_i in range(3, below_limit):
+        if num_i % 3 == 0 or num_i % 5 == 0:
+            yield num_i
+
+
+if __name__ == "__main__":
+    print(sum(multiples_of_3_and_5(below_limit=1000)))
---
+++ python/p002.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: <encoding name> -*-
+from __future__ import unicode_literals, print_function
+
+
+def fib(num):
+    if num == 0 or num == 1:
+        return 1
+    return fib(num-1) + fib(num-2)
+
+
+def fibonacci_sequence(limit):
+    num_i = 0
+    fib_num_i = 1
+    while fib_num_i < limit:
+        fib_num_i = fib(num_i)
+        yield fib_num_i
+        num_i += 1
+
+
+if __name__ == "__main__":
+    fibonacci_numbers = fibonacci_sequence(4000000)
+    fibonacci_sequence_sum = sum(filter(lambda item: item % 2 == 0, fibonacci_numbers))
+    print(fibonacci_sequence_sum)
